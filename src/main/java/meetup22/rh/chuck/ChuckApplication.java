26e436cf6104cf58a627505c5ca1e09add55bb8b
Dmitry Kartsev <dkartsev@redhat.com>
2019-07-17 20:20:52 +0300

    update git url

--- src/main/java/meetup22/rh/chuck/ChuckApplication.java
+++ src/main/java/meetup22/rh/chuck/ChuckApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChuckApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ChuckApplication.class, args);
 	}
