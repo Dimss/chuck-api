213b547b96956f6d5487bb709c90eae4b6c5c191
Dmitry Kartsev <dkartsev@redhat.com>
2019-11-22 13:46:33 +0200

    updates

--- src/main/java/meetup22/rh/chuck/ChuckApplication.java
+++ src/main/java/meetup22/rh/chuck/ChuckApplication.java
@@ -1,8 +1,6 @@
 package meetup22.rh.chuck;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import com.google.gson.stream.JsonReader;
 import meetup22.rh.chuck.model.Joke;
 import meetup22.rh.chuck.repository.JokeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,14 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 @SpringBootApplication
 public class ChuckApplication {
